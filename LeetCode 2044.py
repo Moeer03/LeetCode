2de31d30d94643109419494274c598f479b1b69d
Moeer03 <161110786+Moeer03@users.noreply.github.com>
2025-05-22 21:38:25 +0500

    Update LeetCode 2044.py

--- LeetCode 2044.py
+++ LeetCode 2044.py
@@ -1,10 +1,21 @@
 class Solution:
-    def findArray(self, pref: List[int]) -> List[int]:
-        arr = []
-        prev = 0
+    def countMaxOrSubsets(self, nums):
+        self.max_or = 0
+        self.count = 0
 
-        for i in pref:
-            arr.append(prev ^ i)
-            prev = i
-        
-        return arr
+        def backtrack(i, curr_or):
+            if i == len(nums):
+                if curr_or > self.max_or:
+                    self.max_or = curr_or
+                    self.count = 1
+                elif curr_or == self.max_or:
+                    self.count += 1
+                return
+            # Include nums[i]
+            backtrack(i + 1, curr_or | nums[i])
+            # Exclude nums[i]
+            backtrack(i + 1, curr_or)
+
+        backtrack(0, 0)
+        return self.count
+''
